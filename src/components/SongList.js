23d262cd6f5afe3a6d706a803ee3f94140830c3b
John Raptis <johnjohn_raptis@yahoo.gr>
2019-10-16 13:00:59 +0300

    first-commit

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,5 @@
-import React, { Fragment, useState } from 'react'
-import { useDispatch, useSelector } from "react-redux"
+import React, { Fragment } from 'react'
+import { connect } from "react-redux"
 import { 
 	addSong, 
 	removeSong, 
@@ -8,78 +8,116 @@ import {
 	cancelEdit
 	 } from '../actions/songActions'
 
-const SongList = () => {
-	const dispatch = useDispatch()
-	const [newSong, setNewSong] = useState();
-	const [currentVal, setCurrentVal] = useState();
-	const { songs } = useSelector(state => state.songs)
+class SongList extends React.Component {
 
-	const addNewSong = (e) => {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newSong: '',
+      currentVal: ''
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.remove = this.remove.bind(this);
+    this.edit = this.edit.bind(this);
+    this.update = this.update.bind(this);
+    this.cancel = this.cancel.bind(this);
+    this.updatedVal = this.updatedVal.bind(this);
+  }
+
+	onSubmit(e) {
 		e.preventDefault();
 
 		const addedSong = {
-			title: newSong
+			title: this.state.newSong
 		} 
 
-		if(addedSong.title) {
-			dispatch(addSong(addedSong))
-			setNewSong('')
-		}
+		this.props.addSong(addedSong);
+		this.setState({ newSong: '' });
 	}	
 
-	const remove = (i) => {
-		dispatch(removeSong(i))
+	onChange(e) {
+    	this.setState({ [e.target.name]: e.target.value });
+  	}
+
+	updatedVal(e) {
+    	this.setState({ [e.target.name]: e.target.value });
+  	}
+
+  	remove(i) {
+		this.props.removeSong(i);
 	}
 
-	const update = (i) => {
-		dispatch(updateSong(currentVal, i))
-		setCurrentVal('')
+	edit(i, title) {
+    	this.props.editSong(i);
+    	this.setState({ currentVal: title })
 	}
 
-	const edit = (i, title) => {
-		dispatch(editSong(i))
-		setCurrentVal(title)
+  	update(i) {
+    	this.props.updateSong(this.state.currentVal, i);
+    	this.setState({ currentVal: '' })
 	}
 
-	const cancel = (i) => {
-		dispatch(cancelEdit(i))
+	 cancel(i) {
+    	this.props.cancelEdit(i);
 	}
 
-	return (
-		<ul>
-		{songs.map((song , i) => {
-			return (
-				<Fragment key={song.title}>
-				{(!song.editing) ? (
-				<li>
-				{song.title}
-					<span>
-						<button onClick={() => remove(i)}>Delete</button>
-						<button onClick={() => edit(i, song.title)}>Edit</button>
-					</span>
-				</li>
-					) : (
-				<li>
-					<form>
-						<input type="text" value={currentVal} onChange={e => setCurrentVal(e.target.value)} />
-					</form>
-					<span>
-						<button onClick={() => cancel(i)}>Cancel</button>
-						<button onClick={() => update(i)}>Update</button>
-					</span>
-				</li>
-					)}
-				</Fragment>
-			)
-		})}
-			<form onSubmit={addNewSong}>
-				<input type="text" onChange={e => setNewSong(e.target.value)} />
+	render() {
+		const { songs } = this.props.songs;
+		return (
+			<ul>
+			{songs.map((song , i) => {
+				return (
+					<Fragment key={song.title}>
+					{(!song.editing) ? (
+					<li>
+					{song.title}
+						<span>
+							<button onClick={() => this.remove(i)}>Delete</button>
+							<button onClick={() => this.edit(i, song.title)}>Edit</button>
+						</span>
+					</li>
+						) : (
+					<li>
+ 						<form>
+							<input 
+							type="text" 
+							name="currentVal" 
+							value={this.state.currentVal} 
+							onChange={this.updatedVal}
+							/>
+						</form>
+ 						<span>
+ 							<button onClick={() => this.cancel(i)}>Cancel</button>
+ 							<button onClick={() => this.update(i)}>Update</button>
+						</span>
+					</li>
+						)}
+					</Fragment>
+				)
+			})}
+			<form onSubmit={this.onSubmit}>
+				<input 
+				type="text" 
+				name="newSong" 
+				onChange={this.onChange} 
+				/>
 				<input type="submit" value="Add Song" />
 			</form>
-		</ul>
-	)
+			</ul>
+		);
+	}
 }
 
-export default SongList
+const mapStateToProps = state => ({
+  songs: state.songs
+});
 
-
+export default connect(mapStateToProps, {
+	addSong, 
+	removeSong, 
+	editSong, 
+	updateSong, 
+	cancelEdit 
+})(SongList);
