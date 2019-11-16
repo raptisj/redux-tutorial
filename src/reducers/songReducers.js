70f632804b276027205bf94aa472d92847effbdb
John Raptis <johnjohn_raptis@yahoo.gr>
2019-11-16 19:53:56 +0200

    refactor reducer

--- src/reducers/songReducers.js
+++ src/reducers/songReducers.js
@@ -1,59 +1,51 @@
-import { 
-	ADD_SONG, 
-	DELETE_SONG, 
-	EDIT_SONG, 
+import {
+	ADD_SONG,
+	DELETE_SONG,
+	EDIT_SONG,
 	UPDATE_SONG,
 	CANCEL_EDIT
-} from '../actions/types'
+} from "../actions/types";
 
 const initialState = {
 	songs: [
-		{title: 'I love redux', editing: false},
-		{title: 'The redux song', editing: false},
-		{title: 'Run to the redux hill', editing: false}
+		{ title: "I love redux", editing: false },
+		{ title: "The redux song", editing: false },
+		{ title: "Run to the redux hill", editing: false }
 	]
-}
+};
 
 export default function(state = initialState, action) {
-	switch(action.type) {
+	switch (action.type) {
 		case ADD_SONG:
 			return {
-				songs: [action.payload, ...state.songs]	
-			} 
+				songs: [action.payload, ...state.songs]
+			};
 		case DELETE_SONG:
 			return {
 				songs: state.songs.filter((s, i) => i !== action.payload)
-			}
+			};
 		case EDIT_SONG:
 			return {
-				songs: state.songs.filter((song, i) => {
-					if(i === action.payload) {
-						 song.editing = true 
-					} else {
-						song.editing = false
-					}
-					return song
-				})
-			}
+				songs: state.songs.map((song, i) =>
+					i === action.payload
+						? { ...song, editing: true }
+						: { ...song, editing: false }
+				)
+			};
 		case UPDATE_SONG:
 			return {
-				songs: state.songs.filter((song, i) => { 
-					if(i === action.index) {
-				 		song.title = action.title 
-						song.editing = false
-					}
-					return song
-				})
-			}
+				songs: state.songs.map((song, i) =>
+					i === action.index
+						? { ...song, title: action.title, editing: false }
+						: song
+				)
+			};
 		case CANCEL_EDIT:
 			return {
-				songs: state.songs.map((song, i) => {
-					if(i === action.index) {
-						song.editing = false
-					}
-					return song
-				})
-			}
+				songs: state.songs.map((song, i) =>
+					i === action.index ? { ...song, editing: false } : song
+				)
+			};
 		default:
 			return state;
 	}
